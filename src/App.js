34321c3d1a6ae88bf3e8c1e4f85e52802cde6173
gamze <gamzekandara@gmail.com>
2024-01-26 14:17:12 -0500

    Adds notes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
+// it will render the Article component first, then Comment which will return 2 div HTML elements
+// App :  parent Children : Article and Comment 
 function App() {
   return (
     <div>
--- src/Article.js
+++ src/Article.js
@@ -2,6 +2,9 @@ import React from "react";
 
 function Article() {
   return (
+    // document.createElement('div')
+    // div.innerText = .....
+    // React creates div element and appends it to virtual DOM.
     <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
   );
 }
